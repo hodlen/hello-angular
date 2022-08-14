7390da67f0b32b83588bd8097abf4a4ace8f2b60
Holden <34213478+hodlen@users.noreply.github.com>
2022-08-14 19:16:15 +0800

    add static todos

---
+++ src/app/todos.ts
@@ -0,0 +1,32 @@
+export type Todo = {
+  content: String;
+  subtasks: Todo[];
+};
+
+export const staticTodos: Todo[] = [
+  {
+    content: 'Work on the landing page',
+    subtasks: [],
+  },
+  {
+    content: 'Buy food',
+    subtasks: [],
+  },
+  {
+    content: 'Design a banner',
+    subtasks: [],
+  },
+  {
+    content: 'Cleaning on weekends',
+    subtasks: [
+      {
+        content: 'Declutter phone, laptop',
+        subtasks: [],
+      },
+      {
+        content: 'Get a mattress',
+        subtasks: [],
+      },
+    ],
+  },
+];
