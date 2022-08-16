ecb2251074ee90447643d190c83ba461893c7cb4
Holden <34213478+hodlen@users.noreply.github.com>
2022-08-16 10:57:57 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { staticTodos, Todo, name } from "./todos";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'hello-angular';
+  public name: String = name;
+  public todos: Todo[] = staticTodos;
 }
--- src/app/todos.ts
+++ src/app/todos.ts
@@ -5,28 +5,35 @@ export type Todo = {
 
 export const staticTodos: Todo[] = [
   {
-    content: 'Work on the landing page',
-    subtasks: [],
+    content: "Work on the landing page",
+    subtasks: []
   },
   {
-    content: 'Buy food',
-    subtasks: [],
+    content: "Buy food",
+    subtasks: []
   },
   {
-    content: 'Design a banner',
-    subtasks: [],
+    content: "Design a banner",
+    subtasks: []
   },
   {
-    content: 'Cleaning on weekends',
+    content: "Cleaning on weekends",
     subtasks: [
       {
-        content: 'Declutter phone, laptop',
-        subtasks: [],
+        content: "Declutter phone, laptop",
+        subtasks: []
       },
       {
-        content: 'Get a mattress',
-        subtasks: [],
-      },
-    ],
-  },
+        content: "Get a mattress",
+        subtasks: [
+          {
+            content: "Blah Blah",
+            subtasks: []
+          }
+        ]
+      }
+    ]
+  }
 ];
+
+export const name = "hhhh";
